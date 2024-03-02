e5107da28298e6188d08d3bfc2e3961091cf2e9b
Ali Tulegenov <220264@astanait.edu.kz>
2024-03-02 20:02:25 +0500

    make it better

--- server.js
+++ server.js
@@ -4,9 +4,21 @@ const path = require('path');
 const routes = require('./routes/index');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+const helmet = require('helmet');
 
 
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", "*"],
+      connectSrc: ["'self'", "*", "http://localhost:3000"], 
+      imgSrc: ["'self'", 'data:',"*"], 
+      scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "*", "https://cdn.jsdelivr.net", "https://ajax.googleapis.com"], 
+      scriptSrcAttr: ["'unsafe-inline'"] 
+    }
+  })
+);
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -27,6 +39,6 @@ mongoose.connect(uri)
 
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
